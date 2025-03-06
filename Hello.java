14b910a4e5e600fcf8c302c8364f094529d426c4
Sara0206ferreira <sfdsilva1@minha.fag.edu.br>
2025-03-06 19:31:24 -0300

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
+        helloWorld();
+    }
+    public static void helloWorld() {
         System.out.println("Hello World");
     }
 }
-    public static void helloWorld {
-        System.out.println("Hello World");
-    }
